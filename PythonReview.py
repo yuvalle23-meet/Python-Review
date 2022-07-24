e222e3890b2410133bf0c04f615d141e8af9b4c3
Yuval Levi <yuval111@ironihei.tad.org.il>
2022-07-24 18:21:01 +0300

    Update PythonReview.py

--- PythonReview.py
+++ PythonReview.py
@@ -1,8 +1,8 @@
 
 
 
-def create_youtube_video(title, description): 
-	youtubevideo = {"Title":title, "Description":description, "Likes":0, "Dislikes":0, "Comments":{}}
+def create_youtube_video(title, description, hashtags): 
+	youtubevideo = {"Title":title, "Description":description, "Likes":0, "Dislikes":0, "Comments":{}, "Hashtag":hashtags}
 	return youtubevideo
 
 def like(dic):
@@ -20,9 +20,20 @@ def add_comment(dic, username, text):
 	print(dic)
 	return dic
 
+def similarity(dic, dic1):
+	percnum = 0
+	for a in dic1["Hashtag"]:
+		for b in dic["Hashtag"]:
+			if a==b:
+				percnum += 20;
+	return percnum
+
 tit = "yeet"
 desc = "Cool"
-dic = create_youtube_video(tit, desc)
+hasht = ["Cool", "yeet", "amazing", "asdasdas", "asd"]
+dic = create_youtube_video(tit, desc, hasht)
+dic1 = create_youtube_video("yeetus", "cooles", ["Cool", "yeet", "a", "b", "c"])
 like(dic)
 dislike(dic)
 add_comment(dic, "AwesomeTuba682", "You're bad")
+print(similarity(dic, dic1))
